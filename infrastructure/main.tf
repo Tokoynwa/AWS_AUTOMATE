edfebb2f8db0abca4c1332306f72f6536feab18d
Tornike Shioshvili <tornikeshioshvili1997@gmail.com>
2025-03-22 13:45:00 +0400

    terra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,36 +1,26 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "AWS-Cluster"
+  name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+    subnet_ids = [
+      aws_subnet.subnet1.id,
+      aws_subnet.subnet2.id
+    ]
   }
 }
 
-resource "aws_eks_node_group" "node_group" {
+resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "worker-node-medium"
-  node_role_arn   = aws_iam_role.node_group_role.arn
-
-  subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 
   scaling_config {
     desired_size = 1
-    max_size     = 2
+    max_size     = 1
     min_size     = 1
   }
 
   instance_types = ["t3.medium"]
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-tokoynwa"
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
 }
--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-tokoynwa"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
