4b5870cb4ad9ed159821c8a45e427827ed098b8a
Tornike Shioshvili <tornikeshioshvili1997@gmail.com>
2025-03-22 13:19:34 +0400

    terra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,34 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  name     = "AWS-Cluster"
+  role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
+    subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
   }
-
-  depends_on = [aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy]
 }
 
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "worker-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
+  node_group_name = "worker-node-medium"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+
+  subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
 
   scaling_config {
     desired_size = 1
-    max_size     = 1
+    max_size     = 2
     min_size     = 1
   }
 
-  instance_types = [var.node_instance_type]
+  instance_types = ["t3.medium"]
+}
 
-  remote_access {
-    ec2_ssh_key = "my-ssh-key"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-tokoynwa"
+  acl    = "private"
+  versioning {
+    enabled = true
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly
-  ]
 }
--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-tokoynwa"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
